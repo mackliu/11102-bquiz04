e5f582eacd5e3c4dddc1271d315556c21af6c7f2
mackliu <mackliu@wda.gov.tw>
2023-02-17 14:40:58 +0800

    完成商品列表頁面及按鈕功能定義

--- back/th.php
+++ back/th.php
@@ -84,3 +84,36 @@ $.get("./api/get_bigs.php",(bigs)=>{
 <div class="ct">
     <button onclick="location.href='?do=add_goods'">新增商品</button>
 </div>
+
+<table class="all">
+    <tr class="tt ct">
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>庫存量</td>
+        <td>狀態</td>
+        <td>操作</td>
+    </tr>
+
+
+<?php
+$rows=$Goods->all();
+foreach($rows as $row){
+?>
+    <tr class="pp ct">
+        <td><?=$row['no'];?></td>
+        <td><?=$row['name'];?></td>
+        <td><?=$row['stock'];?></td>
+        <td><?=($row['sh']==1)?'販售中':'已下架';?></td>
+        <td>
+            <button onclick="location.href='?do=edit_goods&id=<?=$row['id'];?>'">修改</button>
+            <button onclick="del('Goods',<?=$row['id'];?>)">刪除</button>
+            <button onclick="sh('up',<?=$row['id'];?>)">上架</button>
+            <button onclick="sh('down',<?=$row['id'];?>)">下架</button>
+
+        </td>
+    </tr>
+<?php
+}
+?>
+
+</table>
