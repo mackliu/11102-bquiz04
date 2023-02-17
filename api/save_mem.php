386a50974a2c4f692fa2503a273fb44e9fc77b84
mackliu <mackliu@wda.gov.tw>
2023-02-17 08:51:42 +0800

    完成會員管理功能

--- api/save_mem.php
+++ api/save_mem.php
@@ -1,7 +1,12 @@
 <?php include_once "base.php";
 
 
-$_POST['reg_date']=date("Y-m-d");
+if(!isset($_POST['id'])){
+    $_POST['reg_date']=date("Y-m-d");
+    $Mem->save($_POST);
+    exit();
+}
 
 $Mem->save($_POST);
 
+to("../back.php?do=mem");
